c0b269e8d77d90099abf0c4c9064997bae16e70e
Beatriz <beatriz.alencar@sesatech.com.br>
2026-01-11 13:18:28 -0300

    add exercise 4

---
+++ FundamentosLogicaDeProgramacao/src/exercicio4/Main.java
@@ -0,0 +1,36 @@
+package exercicio4;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/**
+		 * Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por
+hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas
+decimais
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		int numberFunc;
+		int horas;
+		double salario;
+		double horaValor;
+		
+		System.out.println("Qual seu número de matrícula?");
+		numberFunc = sc.nextInt();
+		System.out.println("Quantas horas você trabalhou essa semana?");
+		horas = sc.nextInt();
+		System.out.println("Quanto vale sua hora de trabalho?");
+		horaValor = sc.nextDouble();
+		double resultado = ((horas) * horaValor);
+		salario = resultado;
+		System.out.printf("A matrícula do funcionário= %d%n", numberFunc);
+		System.out.printf("A quantidade de horas trabalhadas: %d%n", horas);
+		System.out.printf("O valor do salário semanal é: R$ %.2f", salario);
+		
+		sc.close();
+
+	}
+
+}
