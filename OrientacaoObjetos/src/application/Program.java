b9695abff2ee6aebb4d203085985be9a59a7943b
Beatriz <beatriz.alencar@sesatech.com.br>
2026-01-12 19:37:56 -0300

    add calculateArea method in Triangle class

--- OrientacaoObjetos/src/application/Program.java
+++ OrientacaoObjetos/src/application/Program.java
@@ -11,11 +11,10 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		Triangle x, y;
 		x = new Triangle();
 		y = new Triangle();
-	
 
 		System.out.println("Digite as medidas de cada lado do triângulo X:");
 		x.a = sc.nextDouble();
@@ -26,11 +25,8 @@ public class Program {
 		y.b = sc.nextDouble();
 		y.c = sc.nextDouble();
 
-		double p = (x.a + x.b+ x.c) / 2;
-		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-
-		p = (y.a + y.b + y.c) / 2;
-		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+		double areaX = x.calculeArea();
+		double areaY = y.calculeArea();
 
 		System.out.printf("Área do Triângulo X: %.4f%n", areaX);
 		System.out.printf("Área do Triângulo Y: %.4f%n ", areaY);
